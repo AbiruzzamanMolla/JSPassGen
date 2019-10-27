750a4d7a61e99b96ada133250fff7059d948260c
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-10-27 12:16:10 +0600

    random function selector

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
 
-
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
 
 // Generate Function
 
