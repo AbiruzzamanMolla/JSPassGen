fe4d74e2e927453fe67a10e7c4495f953b65e70a
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-10-27 14:12:39 +0600

    Project Completed

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ generateEl.addEventListener('click', () => {
     const hasNumber = numbersEl.checked;
     const hasSymbol = symbolsEl.checked;
     //show the pwd into textbox
-    resultEl.innerText =  generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
 
 //function to suffle the pwd
@@ -32,17 +32,51 @@ function randomsort(a, b) {
     return Math.random() > 0.5 ? -1 : 1;
 }
 
+//copy to clipboard
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    // designed Notification panel
+    window.createNotification({
+        closeOnClick: true,
+        positionClass: 'nfc-top-right',
+        showDuration: 3500,
+        theme: 'success'
+    })({
+        title: 'Notification',
+        message: 'Password copied to clipboard'
+    });
+});
+
 // function for generating password
 function generatePassword(lower, upper, number, symbol, length) {
     let generatedPwd = '';
-    const typeCount = lower+upper+number+symbol;
-    const typeArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    const typeCount = lower + upper + number + symbol;
+    const typeArr = [{
+        lower
+    }, {
+        upper
+    }, {
+        number
+    }, {
+        symbol
+    }].filter(
         item => Object.values(item)[0]
     );
-    if(typeCount === 0){
+    if (typeCount === 0) {
         return '';
     }
-    for(let i = 0; i < length; i+= typeCount){
+    for (let i = 0; i < length; i += typeCount) {
         typeArr.forEach(type => {
             const funcName = Object.keys(type)[0];
             generatedPwd += randomFunc[funcName]();
@@ -53,18 +87,18 @@ function generatePassword(lower, upper, number, symbol, length) {
 }
 
 // Lower Case alpha generator
-function getRandomLower(){
+function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 // Upper Case alpha generator
-function getRandomUpper(){
+function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 // Number Case alpha generator
-function getRandomNumber(){
+function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 // Symbols Case alpha generator
-function getRandomSymbol(){
+function getRandomSymbol() {
     return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
 }
