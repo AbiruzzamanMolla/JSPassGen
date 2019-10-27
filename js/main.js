d25772ef314cbbd9a505f46aae7cbcd408fdb1bf
Md Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2019-10-27 11:52:46 +0600

    Function for generation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("Hello from JS!");
+
+
+
+
+// Generate Function
+
+// Lower Case alpha generator
+function getRandomLower(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// Upper Case alpha generator
+function getRandomUpper(){
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// Number Case alpha generator
+function getRandomNumber(){
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// Symbols Case alpha generator
+function getRandomSymbol(){
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+
+console.log(getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol());
